cf29a65c0eac23911c9a8e1b275df967d215263b
Jathin P <jathinp74@gmail.com>
2026-07-16 00:28:10 +0530

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,14 +9,8 @@ function Navbar() {
         <nav className="navbar-links">
           <a href="#how-it-works">How it works</a>
           <a href="#cuisines">Cuisines</a>
-          <a href="#cities">Cities</a>
           <a href="#about">About</a>
-        </nav>
-
-        <div className="navbar-actions">
-          <a href="#signin" className="link-muted">Sign in</a>
-          <a href="#book" className="btn btn-primary btn-sm">Book a cook</a>
-        </div>
+        </nav>      
       </div>
     </header>
   )
